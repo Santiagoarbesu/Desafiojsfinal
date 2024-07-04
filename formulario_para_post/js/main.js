5b26a33ab000c792fbe6e25198a450d5c408ec33
Valeria Elias <svaleria.elias.palacios@gmail.com>
2024-07-04 12:34:38 -0600

    added functionality to the form so it shows on the postcard

--- formulario_para_post/js/main.js
+++ formulario_para_post/js/main.js
@@ -3,69 +3,99 @@ CreatePostForm.addEventListener("submit", (form) => {
     form.preventDefault();
   
     // extrae la informacion de los imputs, preventDefault the default action that belongs to the event will not occur
-  });
+  
   let postCoverImage = document.getElementById("post-cover-image");
   let postTitle= document.getElementById("post-title");
-  let imputHashtags= document.getElementById("input-hashtags");
+  let inputHashtags= document.getElementById("input-hashtags");
   let postContent= document.getElementById("post-content");
 
 
-  if (postCoverImage.value == "" || postTitle.value == "" || imputHashtags.value == "" || postContent.value =="" ) {
+  if (postCoverImage.value == "" || postTitle.value == "" || inputHashtags.value == "" || postContent.value =="" ) {
     alert("Ensure you input a value in all fields!");
   } else {
     // perform operation with form input
     alert("This form has been successfully submitted!");
-    console.log(
+    /*console.log(
       `This form has this cover-image of ${postCoverImage.value} a post-title of ${postTitle.value} this hashtags ${imputHashtags.value} and this post-content ${postContent.value} `
-    );
+    );*/
 
-    postCoverImage.value = "";
-    postTitle.value = "";
-    imputHashtags.value = "";
-    postContent.value = "";
+    
+    //let emptyPost = {};
+    let comentNumber= `${getRandomNumber (2,200)} coments`
+    console.log(comentNumber)
+    
+    let reactionNumber= `${getRandomNumber (2,200)} reactions`
+    console.log(reactionNumber)
+    
+    //Agregar los comentarios y reacciones al post
+   
+    
+
+    let emptyPost = {
+      postCoverImage : postCoverImage,
+      postTitle : postTitle,
+      inputHashtags : inputHashtags,
+      postContent : postContent,
+      coments : comentNumber,
+      reactions : reactionNumber
+    }
+
+   
+
+//console.log(emptyPost);
+updatePreview(emptyPost);
+
+
   };
-  let createPostForm = document.querySelectorAll("#user-form-create-post .post-field");
-
-console.log();
-
-  let emptyPost = {};
-
-/*a cada campo le agregamos un listener*/
-createPostForm.forEach((field) => {
-  field.addEventListener("keyup", (event) => {
-    /*De cada campo, extraeos loq ue tiene escrito, y su nombre, lo que tiene escrito representa el valor de la propiedad, y el nombre del campo representa la propiedad que queremos crear en el  objeto*/
-    let value = event.target.value;
-    let property = event.target.name;
-
-    console.log("property: ", property);
-    console.log("value: ", value);
-
-    /*Asingamos la propiedad y su valor al objeto*/
-    emptyPost[property] = value;
-    console.log("entryObject: ", emptyPost);
-
-    /*usamos el objeto actualizado, para modificar la vista previa de nuestra entrada de blog ( ver la función updatePreview que esta más arriba)*/
+  
   });
-});
+
+
+
+  
 //Agregar comentarios y reacciones al formulario
 
 /* Generar numeros aleatorios*/
 function getRandomNumber (min,max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let comentNumber= `${getRandomNumber (2,200)} coments`
-console.log(comentNumber)
 
-let reactionNumber= `${getRandomNumber (2,200)} reactions`
-console.log(reactionNumber)
 
-//Agregar los comentarios y reacciones al post
-emptyPost.coments=comentNumber
-emptyPost.reactions=reactionNumber
-console.log(emptyPost)
 
+//Enlazar el contenido de los inputs a la tarjeta de Post
+/*Esta función modifica la card que está en nuestro HTML*/
+ const updatePreview = (entryData) => {
+ // console.log(entryData);
+  /*Recibe como argumento entryData, que representa el objeto de nuestra entrada de blog*/
+
+  /*Extraemos los valores de title, content, cover, que usaremos para modificar nuestro HTML*/
+  //let { coverImage, title, hashtags, content, coments, reactions } = entryData;
+let coverImage = entryData.postCoverImage.value;
+let title = entryData.postTitle.value;
+let hashtags = entryData.inputHashtags.value;
+let content = entryData.postContent.value;
+let coments = entryData.coments;
+let reactions = entryData.reactions;
+
+console.log(coments);
+  /*Seleccionamos los elementos de html que vamos a modificar. Estos elementos se encuentran en el index.html*/
+  let coverElement = document.getElementById("card-post-cover-image"); /*la imagen*/
+  let titleElement = document.getElementById("card-post-title"); /*el título*/
+  let hashtagsElement = document.getElementById("card-input-hashtags"); /*las etiquetas*/
+  /*let contentElement =document.getElementById("card-post-content"); /*el contenido*/
+  let comentsElement = document.getElementById("card-coments"); /*comentarios*/
+  let reactionsElement = document.getElementById("card-reactions"); /*reacciones*/
+console.log(comentsElement);
+  /*cambiamos el contenido de cada elemento, usando lo que tenemos en entryData*/
+  coverElement.setAttribute("src", coverImage);
+  titleElement.textContent = title;
+  hashtagsElement.textContent = hashtags;
+  /*contentElement.textContent = content;*/
+  comentsElement.innerHTML = coments;
+  reactionsElement.innerHTML = reactions;
+};
 //Generar botones de tags
-let tagsInput = document.getElementById ("input-hashtags");
+/*let tagsInput = document.getElementById ("input-hashtags");
 let tagsWrapper = document.getElementById("tags-wrapper");
 
 let tagsArray = [];
@@ -97,4 +127,4 @@ const printTagButtons = (tagsArray) => {
         return'button class="btn btn-primary">#${current}</button>';
     })
     tagsWrapper.innerHTML = tagsHtml;
-};
+};*/
